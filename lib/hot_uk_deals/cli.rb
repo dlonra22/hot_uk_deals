1ba048bd855d3f6fc372ae1c5c78756d26fa257a
arnie b <arnold.bwaila@gmail.com>
2020-01-27 23:04:46 +0000

    First Changes

---
+++ lib/hot_uk_deals/cli.rb
@@ -0,0 +1,8 @@
+
+require "hot_uk_deals/version.rb"
+class HotUkDeals::CLI
+  
+  def call
+    puts "Welcome to Hot Uk Deals where you can find hot deals, New Deals and Trending deals around the UK"
+  end
+  end
